540d4a3f068cbce895ac47275c09cff77e350069
ovais008 <ovais@revsmith.ai>
2024-11-28 19:01:57 +0500

    Changed environment variable for version

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 async function sendWhatsAppMessage(to, message) {
     try {
         const response = await axios.post(
-            `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
+            `https://graph.facebook.com/${process.env.VERSION}/${process.env.PHONE_NUMBER_ID}/messages`,
             {
                 messaging_product: "whatsapp",
                 to: to,
